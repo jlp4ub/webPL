74c31130f6eb34dbff11130887676f448ac91ffa
jlp4ub <36249195+jlp4ub@users.noreply.github.com>
2019-04-05 17:23:50 -0400

    Update index.php

--- Project/cs4640/index.php
+++ Project/cs4640/index.php
@@ -34,6 +34,77 @@
 </head>
 
 <body>
+    
+    <?php
+    session_start();
+
+    // LOG IN ~ CHECK IF EMAIL IS FOUND IN DATABASE
+    require('connect-db.php');
+    // require('database.php');
+    $email = "";
+    $password = "";
+    // if (isset($_SESSION['use'])) {
+    //     header('Location: index3.php');
+    // }
+    
+    if (isset($_POST['lname']) && isset($_POST['lpass'])) {
+        $email = $_POST['lname'];
+        $password = $_POST['lpass'];
+
+        $query = "SELECT accounts FROM email WHERE email='$email'";
+        $res_u = mysqli_query($db, $query);
+        echo 'hello' . $res_u;
+
+        if (mysqli_num_rows($res_u) == 0) {
+            // echo "Account does not exist...Please create an account.";
+            echo "<script type='text/javascript'>alert('Account does not exist...Please create an account.');</script>";
+        } else {
+            $query2 = "SELECT accounts FROM email,password WHERE email='$email' AND password='$password'";
+            $res_p = mysqli_query($db, $query2);
+
+            if (mysqli_num_rows($res_p) == 0) {
+                echo "<script type='text/javascript'>alert('Password does not match email.');</script>";
+            } else {
+                setcookie('email', $email, time()+3600);
+                setcookie('password', md5($password), time()+3600); 
+                $_SESSION['email'] = $email;
+                $_SESSION['password'] = $password;
+                $_SESSION['use'] = $email;
+                        
+                header('Location: index3.php'); 
+            }
+        }
+    }
+
+    //CREATE ACCOUNT ~ CHECK IF EMAIL IS ALREADY IN USE IN DATABASE
+    if (isset($_POST['cname']) && isset($_POST['cpass'])) {
+        $email = $_POST['cname'];
+        $password = $_POST['cpass'];
+
+        $query = "SELECT accounts FROM email WHERE email='$email'";
+        $res_u = mysqli_query($db, $query);
+
+        if (mysqli_num_rows($res_u) > 0) {
+            // echo "Account does not exist...Please create an account.";
+            echo "<script type='text/javascript'>alert('Email is already in use.');</script>";
+        }
+
+        setcookie('email', $email, time()+3600);
+        setcookie('password', md5($password), time()+3600); 
+        $_SESSION['email'] = $email;
+        $_SESSION['password'] = $password;
+        $_SESSION['create'] = $_POST['create'];
+        $_SESSION['school'] = $_POST['school'];
+        $_SESSION['major'] = $_POST['major'];
+        $_SESSION['minor'] = $_POST['minor'];
+
+        $_SESSION['use'] = $email;
+                        
+        header('Location: index3.php'); 
+    }
+    ?>
+
+
 
     <nav class="navbar navbar-expand-md navbar-dark" style="background-color: #61C2A2">
         <a class="navbar-brand" href="#">Schedule4Me</a>
@@ -55,38 +126,36 @@
                      <h5><img src="check.png" style= "width: 8%;height:8%; margin-right: 30px;">Plan out your semesters accordingly</h5>
                      <h5><img src="check.png" style= "width: 8%;height:8%; margin-right: 30px;">See what classes other students are recommending</h5>
                 </div>
-                
-                <!-- <h5>Plan out your semesters accordingly</h5>
-                <h5>See what classes other students are recommending</h5> -->
             </div>   
         </div>
 
-
+<!-- LOG IN -->
         <div id="login" class="col-md-5" style="float:right;">
-            <form id="returnuser" action="index3.php" onsubmit="return validateLoginForm()" method="post">
+            <form id="returnuser" onsubmit="return validateLoginForm()" method="post">
                 <div class="panel-login">
 
                     <div class="sign-in-title">
                         <h1>Sign in</h1>
                     </div>
                     <div class="login-row" style="margin-left:30%">
-                        <input type="email" id="lname" placeholder="Enter UVa email">
+                        <input type="email" name = "lname" id="lname" placeholder="Enter UVa email">
                     </div>
                     <div class="login-row" style="margin-left:30%;">
-                        <input type="password" name="lpass" placeholder="Enter password" required>
+                        <input type="password" name="lpass" id="lpass" placeholder="Enter password" required>
                         <input type="submit" id="button" value="Submit" style = "background-color: #61C2A2">
                     </div>
 
                 </div>
             </form>
 
-            <form id="returnuser" action="index3.php" onsubmit="return validateCreateForm()" method="post">
+<!-- CREATE ACCOUNT -->
+            <form id="returnuser" onsubmit="return validateCreateForm()" method="POST">
                 <div class="panel-login">
                     <div class="sign-in-title">
                         <h1>Create Account</h1>
                     </div>
                     <div class="login-row" style="margin-left:10%">
-                        <input type="email" id="cname" placeholder="Enter UVa email" style="width: 76.5%;">
+                        <input type="email" name= "cname" id="cname" placeholder="Enter UVa email" style="width: 76.5%;">
                     </div>
                     <div class="login-row" style="margin-left:10%">
                         <input list = "schools" name="school" placeholder="Choose School" style="width: 76.5%;">
@@ -116,9 +185,9 @@
 
                     <div>
                         <div class="login-row" style="margin-left:10%;">
-                        <input type="password" id="cpass" placeholder="Enter password" required>
+                        <input type="password" name= "cpass" id="cpass" placeholder="Enter password" required>
                         <input type="password" id="confirmpass" placeholder="Confirm password" required>
-                        <input type="submit" id="button" value="Submit" style = "background-color: #61C2A2"></div>
+                        <input type="submit" name="create" id="button" value="Submit" style = "background-color: #61C2A2"></div>
                     </div>
                     
                 </div>
@@ -127,48 +196,6 @@
         </div>
     </div>
 
-
-    <?php
-    session_start();
-
-    // LOG IN ~ CHECK IF USERNAME IS FOUND IN DATABASE
-    require('connect-db.php');
-    $username = "";
-    $password = "";
-    if (isset($_SESSION['use']) {
-        header('Location: index3.php');
-    }
-
-    if (isset($_POST['lname']) && isset($_POST['lpass'])) {
-        $username = trim($_POST['lname']);
-        $password = trim($_POST['lpass']);
-
-        $query = "SELECT courses FROM courseID WHERE username='$username'";
-        $res_u = mysql_query($db, $query);
-
-        if (mysqli_num_rows($res_u) = 0) {
-            echo "Account does not exist...Please create an account.";
-        } else {
-            $query2 = "SELECT courses FROM courseID WHERE username='username' AND password='$password'";
-            $res_p = mysql_query($db, $query2);
-
-            if (mysqli_num_rows($res_p) = 0) {
-                echo "Password does not match username.";
-            } else {
-                setcookie('username', $username, time()+3600);
-                setcookie('password', md5($password), time()+3600); 
-                $_SESSION['username'] = $username;
-                $_SESSION['password'] = $password;
-                $_SESSION['use'] = $username;
-                        
-                header('Location: index3.php');   
-            }
-        }
-    }
-
-
-
-	?>
 
 
 </body>
